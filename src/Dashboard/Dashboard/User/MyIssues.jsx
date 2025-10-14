9da3cc20a807d87a0359ce1c0fb7cc9b79733046
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-10-14 18:50:15 +0600

    add details in user panel

--- src/Dashboard/Dashboard/User/MyIssues.jsx
+++ src/Dashboard/Dashboard/User/MyIssues.jsx
@@ -63,7 +63,7 @@ const MyIssues = () => {
             ) : (
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10'>
                     {issues.map(item => (
-                        <div key={item._id} className="card border border-gray-300 bg-white shadow-lg rounded-lg transform transition duration-300 hover:scale-105">
+                        <div key={item._id} className="card border border-gray-300 bg-white shadow-lg rounded-lg">
                             {/* <div className='relative'>
                                 <div className='absolute -top-8 left-1/2 transform -translate-x-1/2'>
                                     <img
@@ -89,6 +89,10 @@ const MyIssues = () => {
                                         <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">Update</button>
                                     </Link>
                                     <button onClick={() => handleDelete(item._id)} className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition">Delete</button>
+
+                                    <Link to={`/details-issue/${item._id}`}>
+                                        <button className='btn border-blue-600 border-2 ml-2'>See Details</button>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
